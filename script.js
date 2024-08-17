f32c6623c3997d6b458a2d45b802a43eca01bd60
javohir1606 <javohirallayorov1606@gmail.com>
2024-08-17 17:29:21 +0500

    main

---
+++ script.js
@@ -0,0 +1,68 @@
+// if doir misollar
+// 1 - misol
+// Uygi-vazifa
+// 1 - misol
+// let year1 = prompt("Tug`ilgan yilingizni kiriting!");
+// let year2 = prompt("Tug`ilgan yilingizni kiriting!");
+// let email = prompt("email kiriting!")
+// let married = prompt("Oila qurganmisiz! Ha yoki Yo`q");
+// function Users(name, year, married, email){
+//   this.name = name;
+//   this.year = year;
+//   this.email = email;
+//   this.married = married;
+//   this.calc = function() {
+//     return 2024 - this.year
+//   }
+//   this.x = function(){
+//     if(married == "Ha"){
+//       return true;
+//     }else{
+//       return false;
+//     }
+//   }
+// }
+// const user1 = new Users("Fayruzber", year1, email, married);
+// console.log(user1);
+// console.log(user1.calc());
+// console.log(user1.x());
+
+// 2 - misol
+// let userCount = prompt("userlarni soni");
+// let result = [];
+
+// for (let i = 0; i < userCount; i++) {
+//   let obj = {};
+//   let name = prompt("ismingiz");
+//   let age = prompt("yoshingiz");
+//   let id = prompt("id");
+//   obj.name = name;
+//   obj.age = age;
+//   obj.id = id;
+//   result.push(obj);
+// }
+// console.log(result);
+
+// 3 - misol
+// Foydalanuvchi konstruktor funksiyasi
+// function User(name, age, location, sum) {
+//   this.name = name;
+//   this.age = age;
+//   this.location = location;
+//   this.sum = sum;
+// }
+// function calculSum(users) {
+//   let totalSum = 0;
+//   for (const user of users) {
+//       totalSum += user.sum;
+//   }
+//   return totalSum;
+// }
+// const users = [
+//   new User('Ali', 30, 'Tashkent', 50000),
+//   new User('Vali', 28, 'Samarkand', 60000),
+//   new User('Sami', 35, 'Bukhara', 55000)
+// ];
+// const totalSum = calculSum(users);
+// console.log('Umumiy maosh:', totalSum);
+
